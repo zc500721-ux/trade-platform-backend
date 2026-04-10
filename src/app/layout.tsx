a5f33dbc32d1816a9d7f5527b20bccdd91b3823f
zc500721-ux <zc500721@gmail.com>
2026-04-10 23:18:25 +0800

    Create layout.tsx
    create app layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="zh-CN">
+      <body>{children}</body>
+    </html>
+  );
+}
